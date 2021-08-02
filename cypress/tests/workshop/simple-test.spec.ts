754024bd8ad749b50abd0b87473d7cfc1c133402
kwiebe <katharina.wiebe@maibornwolff.de>
2021-08-02 18:21:41 +0200

    simple test

---
+++ cypress/tests/workshop/simple-test.spec.ts
@@ -0,0 +1,10 @@
+describe("first try", () => {
+    it("SHOULD open the page", () => {
+        cy.visit("/");
+        cy.get('#username').should("exist").clear().type("Katharina_Bernier");
+        cy.get('#password').should("exist").clear().type("s3cret");
+        cy.get('.PrivateSwitchBase-input-14').should("be.not.checked");
+        cy.get('[data-test=signin-submit]').should("be.enabled").click();
+        cy.get('.makeStyles-logo-26').should("exist");
+    });
+});
